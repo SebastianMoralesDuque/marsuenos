fcc6be29f1306c3d0b82599d0702044cf4816850
John Doe <danielmorales.arias23@gmail.com>
2023-07-13 00:12:19 -0400

    agregando estilos al footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,11 +14,11 @@ const Footer: React.FC = () => {
   };
 
   return (
-    <footer className="fixed bottom-0 left-0 w-full bg-gray-800 text-white py-4">
-      <div className="container mx-auto flex flex-wrap justify-between">
+    <footer className="fixed bottom-0 left-0 w-full bg-[#010566] text-white py-8">
+      <div className="container mx-auto flex flex-wrap justify-between space-y-8 md:space-y-0 md:flex-row md:space-x-8">
         <div className="w-full sm:w-1/2 lg:w-auto">
           <div className="footer-column">
-            <h3 className="text-2xl text-pink-500">Contacto</h3>
+            <h3 className="text-2xl text-pink-500 mb-4">Contacto</h3>
             <p>Teléfono: +123456789</p>
             <p>Email: info@agenciadeviajes.com</p>
             <p>Dirección: Calle Principal, Ciudad</p>
@@ -26,31 +26,31 @@ const Footer: React.FC = () => {
         </div>
         <div className="w-full sm:w-1/2 lg:w-auto">
           <div className="footer-column">
-            <h3 className="text-2xl text-pink-500">Enlaces útiles</h3>
-            <ul>
+            <h3 className="text-2xl text-pink-500 mb-4">Enlaces útiles</h3>
+            <ul >
               <li><a href="#">Inicio</a></li>
               <li><a href="#">Destinos</a></li>
               <li><a href="#">Paquetes</a></li>
               <li><a href="#">Promociones</a></li>
               <li><a href="#">Contacto</a></li>
             </ul>
-          </div>
+          </div>text-pink-500
         </div>
         <div className="w-full sm:w-1/2 lg:w-auto">
           <div className="footer-column">
-            <h3 className="text-2xl text-pink-500">Suscríbete</h3>
+            <h3 className="text-2xl text-pink-500 mb-4">Suscríbete</h3>
             <p>Recibe nuestras últimas ofertas por correo electrónico:</p>
-            <div className="flex">
+            <div className="flex mt-2">
               <input
                 type="email"
                 value={email}
                 onChange={handleEmailChange}
                 placeholder="Correo electrónico"
-                className="border border-gray-300 rounded px-4 py-2 mt-2 mr-2"
+                className="border border-gray-300 rounded px-4 py-2 mr-2 w-full"
               />
               <button
                 onClick={handleSubscribe}
-                className="bg-pink-500 hover:bg-pink-600 text-white rounded px-4 py-2 mt-2"
+                className="bg-pink-500 hover:bg-pink-600 text-white rounded px-4 py-2"
               >
                 Suscribirse
               </button>
@@ -59,7 +59,7 @@ const Footer: React.FC = () => {
         </div>
       </div>
       <div className="container mx-auto">
-        <p className="text-center">© Derechos de autor Mar Sueños {new Date().getFullYear()}. Todos los derechos reservados. Política de privacidad | Términos de uso</p>
+        <p className=" text-pink-500 text-center mt-8">© Derechos de autor Mar Sueños {new Date().getFullYear()}. Todos los derechos reservados. Política de privacidad | Términos de uso</p>
       </div>
     </footer>
   );
