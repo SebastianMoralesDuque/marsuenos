30c534ae646500e35ae03360e9452ea7a2986264
John Doe <danielmorales.arias23@gmail.com>
2023-07-13 23:25:32 -0400

    se añadio columnas con iconos y se le añadio estilos al footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { FaPhone, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { IoIosAirplane } from 'react-icons/io';
 
 const Footer: React.FC = () => {
   const [email, setEmail] = useState('');
@@ -15,51 +18,79 @@ const Footer: React.FC = () => {
 
   return (
     <footer className="fixed bottom-0 left-0 w-full bg-[#010566] text-white py-8">
+      {/* Contenedor principal */}
       <div className="container mx-auto flex flex-wrap justify-between space-y-8 md:space-y-0 md:flex-row md:space-x-8">
         <div className="w-full sm:w-1/2 lg:w-auto">
+          {/* Columna de Contacto */}
           <div className="footer-column">
-            <h3 className="text-2xl text-pink-500 mb-4">Contacto</h3>
-            <p>Teléfono: +123456789</p>
-            <p>Email: info@agenciadeviajes.com</p>
-            <p>Dirección: Calle Principal, Ciudad</p>
+            <h3 className="text-2xl text-pink-500 mb-4">Contact</h3>
+            <p>
+              <FaPhone className="inline-block mr-2" /> Phone: +123456789
+            </p>
+            <p>
+              <FaEnvelope className="inline-block mr-2" /> Email:
+              info@travelagency.com
+            </p>
+            <p>
+              <FaMapMarkerAlt className="inline-block mr-2" /> Address: Main
+              Street, City
+            </p>
           </div>
         </div>
         <div className="w-full sm:w-1/2 lg:w-auto">
+          {/* Columna de Enlaces Útiles */}
           <div className="footer-column">
-            <h3 className="text-2xl text-pink-500 mb-4">Enlaces útiles</h3>
-            <ul >
-              <li><a href="#">Inicio</a></li>
-              <li><a href="#">Destinos</a></li>
-              <li><a href="#">Paquetes</a></li>
-              <li><a href="#">Promociones</a></li>
-              <li><a href="#">Contacto</a></li>
+            <h3 className="text-2xl text-pink-500 mb-4">Useful Links</h3>
+            <ul>
+              <li><a href="#" className="text-white hover:text-pink-500 transition-colors duration-300">Home</a></li>
+              <li><a href="#" className="text-white hover:text-pink-500 transition-colors duration-300">Destinations</a></li>
+              <li><a href="#" className="text-white hover:text-pink-500 transition-colors duration-300">Packages</a></li>
+              <li><a href="#" className="text-white hover:text-pink-500 transition-colors duration-300">Promotions</a></li>
+              <li><a href="#" className="text-white hover:text-pink-500 transition-colors duration-300">Contact</a></li>
             </ul>
-          </div>text-pink-500
+          </div>
         </div>
         <div className="w-full sm:w-1/2 lg:w-auto">
+          {/* Columna de Suscripción */}
           <div className="footer-column">
-            <h3 className="text-2xl text-pink-500 mb-4">Suscríbete</h3>
-            <p>Recibe nuestras últimas ofertas por correo electrónico:</p>
+            <h3 className="text-2xl text-pink-500 mb-4">Subscribe</h3>
+            <p>Receive our latest offers by email:</p>
             <div className="flex mt-2">
               <input
                 type="email"
                 value={email}
                 onChange={handleEmailChange}
-                placeholder="Correo electrónico"
+                placeholder="Email"
                 className="border border-gray-300 rounded px-4 py-2 mr-2 w-full"
               />
               <button
                 onClick={handleSubscribe}
                 className="bg-pink-500 hover:bg-pink-600 text-white rounded px-4 py-2"
               >
-                Suscribirse
+                Subscribe
               </button>
             </div>
           </div>
         </div>
+        <div className="w-full sm:w-1/2 lg:w-auto">
+          {/* Columna de Redes Sociales */}
+          <div className="footer-column">
+            <h3 className="text-2xl text-pink-500 mb-4">Follow Us</h3>
+            <div className="flex items-center space-x-4">
+              <a href="#" className="text-white hover:text-pink-500 transition-colors duration-300"><FaFacebook size={30} /></a>
+              <a href="#" className="text-white hover:text-pink-500 transition-colors duration-300"><FaTwitter size={30} /></a>
+              <a href="#" className="text-white hover:text-pink-500 transition-colors duration-300"><FaInstagram size={30} /></a>
+            </div>
+            <div className="flex items-center mt-4">
+              <IoIosAirplane className="text-pink-500 text-7xl" />
+            </div>
+          </div>
+        </div>
       </div>
+      {/* Contenedor inferior */}
       <div className="container mx-auto">
-        <p className=" text-pink-500 text-center mt-8">© Derechos de autor Mar Sueños {new Date().getFullYear()}. Todos los derechos reservados. Política de privacidad | Términos de uso</p>
+        <p className="text-pink-500 text-center mt-8">© Mar Dreams {new Date().getFullYear()}
+          All rights reserved. Privacy Policy | Terms of Use</p>
       </div>
     </footer>
   );
