c6e9d3a31cbf0ae712984045c9e8446994fa2db6
John Doe <danielmorales.arias23@gmail.com>
2023-07-24 22:46:21 -0400

    se agrego el logo a la barra de nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,8 +17,8 @@ const Navbar = () => {
     <nav className="bg-blue-500 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link to="/" className="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Marsueños</span>
+        <img src="/img/logo.png "  className="h-20 w-20 rounded-full mr-3"  alt="Flowbite Logo" />
+        
         </Link>
         <div className="flex md:order-2">
           <button
