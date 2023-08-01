6888c57d1cb4d78750a36b7b442a053ad50e7beb
Sebastian <cheviotin200@gmail.com>
2023-08-01 00:32:57 -0500

    fixed responsive menu and changed color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Login from './Login';
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import Login from "./Login";
 
 const Navbar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation();
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -13,27 +15,31 @@ const Navbar = () => {
     setIsModalOpen(false);
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="bg-blue-500 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-white fixed w-full z-20 top-0 left-0 rounded-md mb-8">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link to="/" className="flex items-center">
-        <img src="/img/logo.png "  className="h-20 w-20 rounded-full mr-3"  alt="Flowbite Logo" />
-        
+          <img src="/img/logo.png" className="h-22 w-48" alt="Flowbite Logo" />
         </Link>
         <div className="flex md:order-2">
           <button
-            type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
             onClick={openModal}
           >
             Inicia sesión
           </button>
+
           <button
             data-collapse-toggle="navbar-sticky"
             type="button"
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-sticky"
-            aria-expanded="false"
+            aria-expanded={isMenuOpen ? "true" : "false"}
+            onClick={toggleMenu}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -53,55 +59,95 @@ const Navbar = () => {
             </svg>
           </button>
         </div>
-        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <div
+          className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+            isMenuOpen ? "block" : "hidden"
+          }`}
+          id="navbar-sticky"
+        >
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-transparent md:flex-row md:space-x-8 md:mt-0 md:border-0 md:dark:border-transparent">
             <li>
               <Link
                 to="/"
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                aria-current="page"
+                className={`block py-2 pl-3 pr-4 rounded md:p-0 text-${
+                  location.pathname === "/" ? "blue-700" : "red-500"
+                } bg-clip-text hover:text-blue-700 ${
+                  location.pathname === "/"
+                    ? " text-blue-700 font-bold"
+                    : "text-blue-700"
+                } md:dark:hover:text-blue-700`}
               >
-                INICIO
+                Inicio
               </Link>
             </li>
             <li>
               <Link
                 to="/about"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className={`block py-2 pl-3 pr-4 rounded md:p-0 text-${
+                  location.pathname === "/about" ? "blue-700" : "red-500"
+                } bg-clip-text hover:text-blue-700 ${
+                  location.pathname === "/about"
+                    ? " text-blue-700 font-bold"
+                    : "text-blue-700"
+                } md:dark:hover:text-blue-700`}
               >
-                LA EMPRESA
+                Empresa
               </Link>
             </li>
             <li>
               <Link
                 to="/services"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className={`block py-2 pl-3 pr-4 rounded md:p-0 text-${
+                  location.pathname === "/services" ? "blue-700" : "red-500"
+                } bg-clip-text hover:text-blue-700 ${
+                  location.pathname === "/services"
+                    ? " text-blue-700 font-bold"
+                    : "text-blue-700"
+                } md:dark:hover:text-blue-700`}
               >
-                SERVICIOS
+                Servicios
               </Link>
             </li>
             <li>
               <Link
-                to="/contact"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                to="/visas"
+                className={`block py-2 pl-3 pr-4 rounded md:p-0 text-${
+                  location.pathname === "/visas" ? "blue-700" : "red-500"
+                } bg-clip-text hover:text-blue-700 ${
+                  location.pathname === "/visas"
+                    ? " text-blue-700 font-bold"
+                    : "text-blue-700"
+                } md:dark:hover:text-blue-700`}
               >
-                VISAS
+                Visas
               </Link>
             </li>
             <li>
               <Link
                 to="/experiences"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className={`block py-2 pl-3 pr-4 rounded md:p-0 text-${
+                  location.pathname === "/experiences" ? "blue-700" : "red-500"
+                } bg-clip-text hover:text-blue-700 ${
+                  location.pathname === "/experiences"
+                    ? " text-blue-700 font-bold"
+                    : "text-blue-700"
+                } md:dark:hover:text-blue-700`}
               >
-                EXPERIENCIAS
+                Experiencias
               </Link>
             </li>
             <li>
               <Link
                 to="/contact"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className={`block py-2 pl-3 pr-4 rounded md:p-0 text-${
+                  location.pathname === "/contact" ? "blue-700" : "red-500"
+                } bg-clip-text hover:text-blue-700 ${
+                  location.pathname === "/contact"
+                    ? " text-blue-700 font-bold"
+                    : "text-blue-700"
+                } md:dark:hover:text-blue-700`}
               >
-                C0NTACTENOS
+                Contactenos
               </Link>
             </li>
           </ul>
